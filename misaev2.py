85f8556af49375686c085a16dd0727d0088d21a5
hazhvalt <hazhvalt@mail.ru>
2025-09-28 22:13:38 +0300

    Добавлена программа анализа чисел: подсчет цифр и суммы цифр

---
+++ misaev2.py
@@ -0,0 +1,17 @@
+[200~numbers = [123, 45, 6789, 0, 100500]
+for num in numbers:
+
+    if num > 0:
+
+        num_str = str(num)
+        count = len(num_str)
+
+
+        total = 0
+        for digit in num_str:
+            total += int(digit)
+
+        print("Число:", num)
+        print("Цифр:", count)
+        print("Сумма цифр:", total)
+        print()
